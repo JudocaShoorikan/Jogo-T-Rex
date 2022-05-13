954eeaa6a6b524f030cdfee9936e06412b0462e7
JudocaShoorikan <thisbranadc@gmail.com>
2022-05-13 16:11:40 -0300

    Jogo T-Rex (Aula 12)

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ var trex, trex_running, edges;
 var groundImage;
 var chao;
 var chao2;
+var nuvem, nuvemImage;
 
 function preload(){
   trex_running = loadAnimation("trex1.png","trex3.png","trex4.png");
   groundImage = loadImage("ground2.png");
+  nuvemImage = loadImage("cloud.png");
 }
 
 function setup(){
@@ -23,25 +25,36 @@ function setup(){
   trex.scale = 0.5;
   trex.x = 50
   chao.addImage("chao", groundImage);
+
 }
 
 function draw(){
-  //definir a cor do plano de fundo 
   background("white");
   
-  //pular quando tecla de espaço for pressionada
   if(keyDown("space") && trex.y > 145){
     trex.velocityY = -8.5;
   }
   
   trex.velocityY = trex.velocityY + 0.5;
-  chao.velocityX = -2
+  chao.velocityX = -3;
   
   if (chao.x <= 0){
     chao.x = chao.width/2;
   }
 
- //impedir que o trex caia
   trex.collide(chao2);
   drawSprites();
+  nuvens();
+}
+
+function nuvens(){
+  if (frameCount %40 == 0){
+    nuvem = createSprite(600, 100, 40, 10);
+    nuvem.scale = 0.7;
+    nuvem.velocityX = -3;
+    nuvem.addImage(nuvemImage);
+    nuvem.y = Math.round (random (10, 100));
+    nuvem.depth = trex.depth
+    trex.depth ++;
+  }
 }
